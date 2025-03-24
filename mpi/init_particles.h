0a745680fbc4ab793fad05ed1dad26000e44b668
Tiago Nóbrega <tiagojnobrega456@gmail.com>
2025-03-24 13:48:04 +0000

    First MPI version

--- mpi/init_particles.h
+++ mpi/init_particles.h
@@ -1,11 +1,10 @@
 #ifndef INIT_PARTICLES_H
 #define INIT_PARTICLES_H
 
+
 typedef struct particle {
-    int cellx, celly;
-    double x, y, vx, vy, m;
-    struct particle *prev, *next;
-    int death_timestamp;
+    double x, y, vx, vy, m, fx, fy;
+    double death_timestamp;
 } particle_t;
 
 typedef struct {
@@ -13,13 +12,44 @@ typedef struct {
     double cmx;
     double cmy;
     int adj_cells[8][2];  // Store (x, y) coordinates of 8 neighbors
-    particle_t *head;
+    particle_t **particles_inside;
+    long long current_size;
+    long long capacity;
 } cell_t;
 
-typedef struct remove_particle {
-    particle_t *particle; 
-    struct remove_particle *next;
-} remove_particle_t;
+typedef struct {
+	int size;
+    int capacity;
+	particle_t *particles;
+} particle_array_t;
+
+typedef struct {
+	int rank;
+	int length_send_buffer;
+	particle_array_t *particles_buffer_send;
+	cell_t* cells_buffer_send;
+	cell_t* cells_buffer_recv;
+	int index;
+	int sent;
+	int received;
+} node_t;
+
+#define PARTICLE_SIZE 8
+#define SIZEOF_PARTICLE(n) (n * PARTICLE_SIZE)
+#define GET_NUMBER_PARTICLE(n) (n / PARTICLE_SIZE)
+
+#define CELL_SIZE 3
+#define SIZEOF_CELL(n) (n * CELL_SIZE)
+#define GET_NUMBER_CELL(n) (n / CELL_SIZE)
+
+// Where n is the number of elements, p the number of processes and id is the rank of the process.
+#define BLOCK_LOW(id, p, n) ((id) * (n) / (p))
+#define BLOCK_HIGH(id, p, n) (BLOCK_LOW((id) + 1, p, n) - 1)
+#define BLOCK_SIZE(id, p, n) (BLOCK_HIGH(id, p, n) - BLOCK_LOW(id, p, n) + 1)
+#define BLOCK_OWNER(index, p, n) (((p) * ((index) + 1) - 1) / (n))
+
+#define CONVERT_TO_LOCAL(id, p, n, x) (x - BLOCK_LOW(id, p, n) + 1)
+
 
 void init_r4uni(int input_seed);
 double rnd_uniform01();
--- mpi/parsim-mpi.c
+++ mpi/parsim-mpi.c
@@ -1,10 +1,440 @@
+#include <mpi.h>
+#include <math.h>
+#include <omp.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <mpi.h>
+#include <string.h>
+
 #include "init_particles.h"
+#define NUM_PARTICLES_BUFFER 10000
 
 
-int main(int argc, char *argv[]) {
+particle_array_t *local_particles;
+particle_t *particle;
 
+enum { TAG_INIT_PARTICLES, TAG_SEND_CENTER_OF_MASS, TAG_SEND_PARTICLES };
+enum { DIAGONAL_UP_LEFT, UP, DIAGONAL_UP_RIGHT, LEFT, RIGHT, DIAGONAL_DOWN_LEFT, DOWN, DIAGONAL_DOWN_RIGHT };
+
+node_t* adjacent_processes[8] = {0};
+
+particle_array_t* particles;
+cell_t** cells;
+
+int myRank, number_processors;
+int size_processor_grid[2] = {0, 0};
+int my_coordinates[2];
+node_t* processes_buffers;
+
+int local_cell_dims[2];
+MPI_Comm cart_comm;
+
+void create_cartesian_communicator(int grid_size) {
+	// Calculate best grid size for the number of processes
+	int periodic[2] = {1, 1};
+
+    MPI_Dims_create(number_processors, 2, size_processor_grid);
+
+	if (size_processor_grid[0] > grid_size) size_processor_grid[0] = grid_size;
+    if (size_processor_grid[1] > grid_size) size_processor_grid[1] = grid_size;
+
+	MPI_Cart_create(MPI_COMM_WORLD, 2, size_processor_grid, periodic, 1, &cart_comm);
+
+	if (size_processor_grid[0] * size_processor_grid[1] <= myRank) {
+		MPI_Barrier(MPI_COMM_WORLD);
+		MPI_Finalize();
+		exit(0);
+	}
+
+	MPI_Comm_rank(cart_comm, &myRank);
+	MPI_Cart_coords(cart_comm, myRank, 2, my_coordinates);
+
+	// Calculate rank adjacent processes
+	int adjacent_processes_rank[8];
+	int counter = 0;
+	for (int i = -1; i <= 1; i++) {
+		for (int j = -1; j <= 1; j++) {
+			if (i == 0 && j == 0) {
+				continue;
+			}
+			int coords[2];
+			coords[0] = my_coordinates[0] + i;
+			coords[1] = my_coordinates[1] + j;
+			MPI_Cart_rank(cart_comm, coords, &adjacent_processes_rank[counter]);
+			counter++;
+		}
+	}
+
+	// Set structure for adjacent cells
+	for (int i = 0; i < 8; i++) {
+		if (adjacent_processes[i] != NULL) {
+			continue;
+		}
+		adjacent_processes[i] = (node_t*)calloc(1, sizeof(node_t));
+		adjacent_processes[i]->rank = adjacent_processes_rank[i];
+
+		for (int j = i + 1; j < 8; j++) {
+			if (adjacent_processes_rank[j] == adjacent_processes_rank[i]) {
+				adjacent_processes[j] = adjacent_processes[i];
+			}
+		}
+	}
+
+	// Populate all processes
+	processes_buffers = (node_t*)calloc(size_processor_grid[0] * size_processor_grid[1], sizeof(node_t));
+	number_processors = size_processor_grid[0] * size_processor_grid[1];
+}
+
+void init_cells(int grid_size) {
+	local_cell_dims[0] = BLOCK_SIZE(my_coordinates[0], size_processor_grid[0], grid_size) + 2;
+	local_cell_dims[1] = BLOCK_SIZE(my_coordinates[1], size_processor_grid[1], grid_size) + 2;
+
+	cells = (cell_t**) malloc(sizeof(cell_t*) * local_cell_dims[0]);
+	cell_t* cells_chunk = (cell_t*) calloc(local_cell_dims[0] * local_cell_dims[1], sizeof(cell_t));
+
+	for (int i = 0; i < local_cell_dims[0]; i++) {
+		cells[i] = &cells_chunk[i * local_cell_dims[1]];
+	}
+}
+
+void delegate_particles(int grid_size, double space_size, long long number_particles, particle_t* particles, particle_array_t* local_particles) {
+	int number_processors_grid = size_processor_grid[0] * size_processor_grid[1];
+	int *counters = (int*)calloc(number_processors_grid, sizeof(int));
+
+	particle_t *buffer_space = (particle_t*)malloc(sizeof(particle_t) * NUM_PARTICLES_BUFFER * (number_processors_grid));
+	particle_t **buffers = (particle_t**)malloc(sizeof(particle_t*) * (number_processors_grid));
+
+    for (long long i = 1; i < number_processors_grid; i++) {
+        buffers[i - 1] = &buffer_space[NUM_PARTICLES_BUFFER * (i - 1)];
+    }
+
+	for (long long i = 0; i < number_particles; i++) {
+		particle_t particle = particles[i];
+
+        // Determine the cell coordinates
+        int cellx = (particle.x / (space_size / grid_size));
+        int celly = (particle.y / (space_size / grid_size));
+
+		int coords_proc_grid[2];
+		coords_proc_grid[0] = BLOCK_OWNER(cellx, size_processor_grid[0], grid_size);
+		coords_proc_grid[1] = BLOCK_OWNER(celly, size_processor_grid[1], grid_size);
+
+		int proc_id_to_send;
+		MPI_Cart_rank(cart_comm, coords_proc_grid, &proc_id_to_send);
+
+        if (proc_id_to_send == 0) {
+            // Check if realloc is needed and add particle to local_particles
+            if (local_particles->size >= local_particles->capacity) {
+                local_particles->capacity *= 2;
+                local_particles->particles = (particle_t*)realloc(local_particles->particles, sizeof(particle_t) * local_particles->capacity);
+            }
+            local_particles->particles[local_particles->size] = particle;
+            local_particles->size++;
+            continue;
+        }
+
+		// Add to process' buffer
+		buffers[proc_id_to_send - 1][counters[proc_id_to_send - 1]] = particle;
+		counters[proc_id_to_send - 1]++;
+
+		// Buffer size reach => send
+		if (counters[proc_id_to_send - 1] == NUM_PARTICLES_BUFFER) {
+			MPI_Send(buffers[proc_id_to_send - 1], SIZEOF_PARTICLE(NUM_PARTICLES_BUFFER), MPI_DOUBLE, proc_id_to_send, TAG_INIT_PARTICLES, cart_comm);
+			counters[proc_id_to_send - 1] = 0;
+		}
+	}
+
+	// Send eveything that is not been sent yet.
+	for (int i = 1; i < number_processors_grid; i++) {
+		if (counters[i - 1] != 0) {
+			MPI_Send(buffers[i - 1], SIZEOF_PARTICLE(counters[i - 1]), MPI_DOUBLE, i, TAG_INIT_PARTICLES, cart_comm);
+		} else {
+			double endFlag[1] = {-1};
+			MPI_Send(endFlag, 1, MPI_DOUBLE, i, TAG_INIT_PARTICLES, cart_comm);
+		}
+	}
+
+	free(counters);
+	free(buffer_space);
+	free(buffers);
+}
+
+void receiveParticles() {
+	int number_doubles;
+	MPI_Status status;
+
+	while (1) {
+		MPI_Probe(0, TAG_INIT_PARTICLES, cart_comm, &status);
+		MPI_Get_count(&status, MPI_DOUBLE, &number_doubles);
+		
+		// Check if it is the end flag
+		if (number_doubles == 1) {
+			break;
+		}
+		int num_particles_received = number_doubles / PARTICLE_SIZE;
+		// Check if realloc is needed
+		if (local_particles->size + num_particles_received > local_particles->capacity) {
+			while (local_particles->size + num_particles_received > local_particles->capacity) {
+                local_particles->capacity *= 2;
+            }
+			local_particles->particles = (particle_t*)realloc(local_particles->particles, sizeof(particle_t) * local_particles->capacity);
+		}
+		// Receive particles
+		MPI_Recv(&(local_particles->particles[local_particles->size]), number_doubles, MPI_DOUBLE, 0, TAG_INIT_PARTICLES, cart_comm, &status);
+		MPI_Get_count(&status, MPI_DOUBLE, &number_doubles);
+		local_particles->size += num_particles_received;
+
+		if (number_doubles / 8 != NUM_PARTICLES_BUFFER) {
+			break;
+		}
+	}
+}
+
+void calculate_centers_of_mass(int grid_size) {
+	// Calculate local center of mass
+	#pragma omp parallel for 
+	for (int i = 0; i < particles->size; i++) {
+		particle_t* particle = &local_particles->particles[i];
+        int global_cell_index_x = particle->x * grid_size;
+        int global_cell_index_y = particle->y * grid_size;
+		int local_cell_index_x = CONVERT_TO_LOCAL(my_coordinates[0], size_processor_grid[0], grid_size, global_cell_index_x);
+		int local_cell_index_y = CONVERT_TO_LOCAL(my_coordinates[1], size_processor_grid[1], grid_size, global_cell_index_y);
+
+		cell_t* cell = &cells[local_cell_index_x][local_cell_index_y];
+
+		#pragma omp atomic
+		cell->mass_sum += particle->m;
+
+		#pragma omp atomic
+		cell->cmx += particle->m * particle->x;
+		#pragma omp atomic
+		cell->cmy += particle->m * particle->y;
+	}
+	// Calculate global center of mass
+	for (int i = 1; i <= local_cell_dims[0] - 2; i++) {
+		for (int j = 1; j <= local_cell_dims[1] - 2; j++) {
+			cell_t* cell = &cells[i][j];
+			if (cell->mass_sum != 0) {
+				cell->cmx /= cell->mass_sum;
+				cell->cmy /= cell->mass_sum;
+			}
+		}
+	}
+}
+
+void send_recv_centers_of_mass() {
+	int cells_size_x = local_cell_dims[0] - 2;
+	int cells_size_y = local_cell_dims[1] - 2;
+	int number_cells_max = cells_size_x * 2 + cells_size_y * 2 + 4;
+
+	// Prepare send buffers
+	for (int i = 0; i < 8; i++) {
+		if (adjacent_processes[i]->cells_buffer_send == NULL) {
+			adjacent_processes[i]->cells_buffer_send = (cell_t*)calloc(number_cells_max, sizeof(cell_t));
+		}
+		if (adjacent_processes[i]->cells_buffer_recv == NULL) {
+			adjacent_processes[i]->cells_buffer_recv = (cell_t*)calloc(number_cells_max, sizeof(cell_t));
+		}
+	}
+
+	// Build buffers to send
+	for (int i = 7; i >= 0; i--) {
+		switch (i) {
+			case DIAGONAL_UP_LEFT:
+				adjacent_processes[i]->cells_buffer_send[adjacent_processes[i]->length_send_buffer] = cells[1][1];
+				adjacent_processes[i]->length_send_buffer++;
+				break;
+			case DIAGONAL_UP_RIGHT:
+				adjacent_processes[i]->cells_buffer_send[adjacent_processes[i]->length_send_buffer] = cells[1][cells_size_y];
+				adjacent_processes[i]->length_send_buffer++;
+				break;
+			case DIAGONAL_DOWN_LEFT:
+				adjacent_processes[i]->cells_buffer_send[adjacent_processes[i]->length_send_buffer] = cells[cells_size_x][1];
+				adjacent_processes[i]->length_send_buffer++;
+				break;
+			case DIAGONAL_DOWN_RIGHT:
+				adjacent_processes[i]->cells_buffer_send[adjacent_processes[i]->length_send_buffer] = cells[cells_size_x][cells_size_y];
+				adjacent_processes[i]->length_send_buffer++;
+				break;
+			case LEFT:
+				for (int j = 1; j <= cells_size_x; j++) {
+					adjacent_processes[i]->cells_buffer_send[adjacent_processes[i]->length_send_buffer] = cells[j][1];
+					adjacent_processes[i]->length_send_buffer++;
+				}
+				break;
+			case RIGHT:
+				for (int j = 1; j <= cells_size_x; j++) {
+					adjacent_processes[i]->cells_buffer_send[adjacent_processes[i]->length_send_buffer] = cells[j][cells_size_y];
+					adjacent_processes[i]->length_send_buffer++;
+				}
+				break;
+			case UP:
+				for (int j = 1; j <= cells_size_y; j++) {
+					adjacent_processes[i]->cells_buffer_send[adjacent_processes[i]->length_send_buffer] = cells[1][j];
+					adjacent_processes[i]->length_send_buffer++;
+				}
+				break;
+			case DOWN:
+				for (int j = 1; j <= cells_size_y; j++) {
+					adjacent_processes[i]->cells_buffer_send[adjacent_processes[i]->length_send_buffer] = cells[cells_size_x][j];
+					adjacent_processes[i]->length_send_buffer++;
+				}
+				break;
+
+			default:
+				printf("[%d] Default case in send send_recv_centers_of_mass\n", myRank);
+				fflush(stdout);
+				break;
+		}
+	}
+
+	// Receive
+	MPI_Request request[8];
+	MPI_Status status[8];
+	for (int i = 0; i < 8; i++) {
+		if (adjacent_processes[i]->received == 1) {
+			request[i] = MPI_REQUEST_NULL;
+			continue;
+		}
+
+		MPI_Irecv(adjacent_processes[i]->cells_buffer_recv, SIZEOF_CELL(number_cells_max), MPI_DOUBLE, adjacent_processes[i]->rank, TAG_SEND_CENTER_OF_MASS, cart_comm, &request[i]);
+		adjacent_processes[i]->received = 1;
+	}
+
+	// Send
+	for (int i = 0; i < 8; i++) {
+		if (adjacent_processes[i]->sent == 1) {
+			continue;
+		}
+
+		MPI_Send(adjacent_processes[i]->cells_buffer_send, SIZEOF_CELL(adjacent_processes[i]->length_send_buffer), MPI_DOUBLE, adjacent_processes[i]->rank, TAG_SEND_CENTER_OF_MASS, cart_comm);
+		adjacent_processes[i]->sent = 1;
+	}
+	MPI_Waitall(1, request, status);
+
+	// Reconstruct data
+	for (int i = 0; i < 8; i++) {
+		switch (i) {
+			case DIAGONAL_UP_LEFT:
+				cells[0][0] = adjacent_processes[i]->cells_buffer_recv[adjacent_processes[i]->index];
+				adjacent_processes[i]->index++;
+				break;
+			case DIAGONAL_UP_RIGHT:
+				cells[0][cells_size_y + 1] = adjacent_processes[i]->cells_buffer_recv[adjacent_processes[i]->index];
+				adjacent_processes[i]->index++;
+				break;
+			case DIAGONAL_DOWN_LEFT:
+				cells[cells_size_x + 1][0] = adjacent_processes[i]->cells_buffer_recv[adjacent_processes[i]->index];
+				adjacent_processes[i]->index++;
+				break;
+			case DIAGONAL_DOWN_RIGHT:
+				cells[cells_size_x + 1][cells_size_y + 1] = adjacent_processes[i]->cells_buffer_recv[adjacent_processes[i]->index];
+				adjacent_processes[i]->index++;
+				break;
+			case LEFT:
+				for (int j = 1; j <= cells_size_x; j++) {
+					cells[j][0] = adjacent_processes[i]->cells_buffer_recv[adjacent_processes[i]->index];
+					adjacent_processes[i]->index++;
+				}
+				break;
+			case RIGHT:
+				for (int j = 1; j <= cells_size_x; j++) {
+					cells[j][cells_size_y + 1] = adjacent_processes[i]->cells_buffer_recv[adjacent_processes[i]->index];
+					adjacent_processes[i]->index++;
+				}
+				break;
+			case UP:
+				for (int j = 1; j <= cells_size_y; j++) {
+					cells[0][j] = adjacent_processes[i]->cells_buffer_recv[adjacent_processes[i]->index];
+					adjacent_processes[i]->index++;
+				}
+				break;
+			case DOWN:
+				for (int j = 1; j <= cells_size_y; j++) {
+					cells[cells_size_x + 1][j] = adjacent_processes[i]->cells_buffer_recv[adjacent_processes[i]->index];
+					adjacent_processes[i]->index++;
+				}
+				break;
+
+			default:
+				printf("[%d] Default case in send Reconstruct Data\n", myRank);
+				fflush(stdout);
+				break;
+		}
+	}
+
+	// Reset structures
+	for (int i = 0; i < 8; i++) {
+		adjacent_processes[i]->cells_buffer_send = NULL;
+		adjacent_processes[i]->cells_buffer_recv = NULL;
+		adjacent_processes[i]->length_send_buffer = 0;
+		adjacent_processes[i]->sent = 0;
+		adjacent_processes[i]->received = 0;
+		adjacent_processes[i]->index = 0;
+	}
+}
+
+void calculate_new_iteration(int grid_size, int space_size) {
+
+	//TODO: Implement this function
+}
+
+void send_recv_particles() {
+
+	//TODO: Implement this function
+}
+
+int main(int argc, char* argv[]) {
+	// Check for the correct number of command-line arguments
+    if (argc != 6) {
+        fprintf(stderr, "Expected 5 arguments, but %d were given\n", argc - 1);
+        return 1;
+    }
+
+    // Parse command-line arguments and initialize variables
+    long seed = atol(argv[1]);
+    double space_size = atof(argv[2]);
+    int grid_size = atoi(argv[3]);
+    long long number_particles = atoll(argv[4]);
+    int n_time_steps = atoi(argv[5]);
+    int collision_count = 0;
+    double exec_time = 0;
+
+	particle_t *particles = (particle_t *)malloc(sizeof(particle_t) * number_particles);
+
+    // Initialize particles with random positions and velocities
+    init_particles(seed, space_size, grid_size, number_particles, particles);
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &number_processors);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+
+	// Local particles array
+	local_particles = (particle_array_t *)malloc(sizeof(particle_array_t));
+	local_particles->size = 0;
+	local_particles->capacity = number_particles * 10 / (grid_size * grid_size);
+	local_particles->particles = (particle_t *)malloc(sizeof(particle_t) * local_particles->capacity);
+
+	create_cartesian_communicator(grid_size);
+	init_cells(grid_size);
+
+	if (myRank == 0) {
+		delegate_particles(grid_size, space_size, number_particles, particles, local_particles);
+	} else {
+		receiveParticles();
+	}
+
+	for (int n = 0; n < n_time_steps; n++) {
+		calculate_centers_of_mass(grid_size);
+		send_recv_centers_of_mass();
+		//calculate_new_iteration(grid_size, space_size);
+		//send_recv_particles();
+
+		memset(cells[0], 0, sizeof(cell_t) * local_cell_dims[0] * local_cell_dims[1]);
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	MPI_Finalize();
+	return 0;
 }
